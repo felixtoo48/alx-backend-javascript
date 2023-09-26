396844b1e6bee17de73f25c690e1bb9132445a50
felixtoo48 <felixtoo48@gmail.com>
2023-09-26 19:57:40 +0300

    simplified line

--- 0x00-ES6_basic/11-createEmployeesObject.js
+++ 0x00-ES6_basic/11-createEmployeesObject.js
@@ -1,6 +1,3 @@
 export default function createEmployeesObject(departmentName, employees) {
-  const departmentObject = {
-    [departmentName]: employees,
-  };
-  return departmentObject;
+  return { [departmentName]: [...employees] };
 }
