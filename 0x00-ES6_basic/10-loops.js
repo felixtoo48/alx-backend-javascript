890f789da2ab88fd1ea3e9d01bfc081d5b6584b7
felixtoo48 <felixtoo48@gmail.com>
2023-09-26 20:08:04 +0300

    fixing lint and semistandard errors

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,4 +1,4 @@
-export default function appendToEachArrayValue(array, appendString) {
+export default function appendToEachArrayValue (array, appendString) {
   for (const value of array) {
     const idx = array.indexOf(value);
     array[idx] = appendString + value; // eslint-disable-line no-param-reassign
