3cc5927f769808dbe1f79592aaaa387c574bbd92
felixtoo48 <felixtoo48@gmail.com>
2023-09-27 16:00:51 +0300

    fixing lint parenthesis error

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,6 +1,6 @@
 // eslint-disable-next-line no-unused-vars
 
-export default function createIteratorObject (report) {
+export default function createIteratorObject(report) {
   const nArr = [];
   for (const arr of Object.keys(report)) {
     nArr.push(arr);
